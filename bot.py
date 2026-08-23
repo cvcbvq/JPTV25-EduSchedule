536f0aa8f5871ebc7124b43981695452f13064f3
cvcbvq <jerjomenkoruslan6@gmail.com>
2026-08-23 17:36:01 +0300

    Add imports for inline keyboard functionality

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import sqlite3
 import asyncio
 from aiogram import Bot, Dispatcher, types 
 from aiogram.filters import Command
-
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, FSInputFile
 
 #ОБЩАЯ ИНИЦИАЛИЗАЦИЯ
 # / ÜLDINE INITSIALISEERIMINE
